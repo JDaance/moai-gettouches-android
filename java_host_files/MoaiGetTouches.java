7a3ac8e648ebb6c946ae19a2e9cfd9b6e16b62fd
Joakim Tall <joakim@fastmail.se>
2014-11-12 08:37:57 +0100

    synchronization issues

--- java_host_files/MoaiGetTouches.java
+++ java_host_files/MoaiGetTouches.java
@@ -12,10 +12,12 @@ public class MoaiGetTouches {
 
 	private static native void AKUSetActiveTouches(int[][] touches);
 
-    // synchronized since events seem to be posted at the same time (rarely)
-	public static synchronized void setLatestMotionEvent(MotionEvent event) {
-        if (latestMotionEvent != null) latestMotionEvent.recycle();
-        latestMotionEvent = MotionEvent.obtain(event);
+	public static void setLatestMotionEvent(MotionEvent event) {
+        // synchronized since events seem to be posted at the same time (rarely)
+        synchronized (MoaiGetTouches.class) {
+            if (latestMotionEvent != null) latestMotionEvent.recycle();
+            latestMotionEvent = MotionEvent.obtain(event);
+        }
 	}
 
     /**
@@ -23,26 +25,40 @@ public class MoaiGetTouches {
      * associated with UP will have been released so don't pass that one to native
      */
     public static void update() {
-        MotionEvent event = latestMotionEvent;
-        if (event == null) return;
+        MotionEvent event;
+        // synchronized copy since events seem to be posted while updating
+        synchronized (MoaiGetTouches.class) {
+            if (latestMotionEvent == null) return;
+            event = MotionEvent.obtain(latestMotionEvent);
+        }
 
         int skipIndex = -1;
         int action = event.getActionMasked();
+        int originalPointerCount = event.getPointerCount();
+        int pointerCount = originalPointerCount;
         if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_POINTER_UP) {
             skipIndex = event.getActionIndex();
+            pointerCount -= 1;
         }
-        int pointerCount = event.getPointerCount() - (skipIndex != -1 ? 1 : 0);
         int[][] touches = new int[pointerCount][3];
         int count = 0;
         for (int i = 0; i < event.getPointerCount(); ++i) {
             if (i != skipIndex) {
-                int idx = event.getPointerId(i);
-                touches[count][IDX] = idx;
-                touches[count][X] = Math.round(event.getX(i));
-                touches[count][Y] = Math.round(event.getY(i));
-                ++count;
+                try {
+                    int idx = event.getPointerId(i);
+                    touches[count][IDX] = idx; // count == 1, touches.length == 1
+                    touches[count][X] = Math.round(event.getX(i));
+                    touches[count][Y] = Math.round(event.getY(i));
+                    ++count;
+                } catch (ArrayIndexOutOfBoundsException ae) {
+                    String message = String.format(
+                            "Out of bounds with skipIndex: %d, originalPointerCount: %d, pointerCount: %d, event.getPointerCount(): %d, count: %d, touches.length: %d, i: %d",
+                            skipIndex, originalPointerCount, pointerCount, event.getPointerCount(), count, touches.length, i);
+                    throw new RuntimeException(message, ae);
+                }
             }
         }
         AKUSetActiveTouches(touches);
+        event.recycle();
     }
 }
